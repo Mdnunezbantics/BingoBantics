7b16d8e4e89060c872877a4ed49e786c5eba7e5b
mdnunez <mdnunez@gmail.com>
2021-08-09 17:13:21 -0300

    asd

--- Funciones/numerador.py
+++ Funciones/numerador.py
@@ -15,7 +15,7 @@ def generar_hoja_carton():
     patron6 = ["2", "3", "4", "7", "9", "10", "11", "14", "16", "18", "19", "21", "23", "26", "27"]
     patrones = [patron1, patron2, patron3, patron4, patron5, patron6]
 
-    nums1 = ["01", "02", "03", "04", "05", "06", "07", "08", "09"]
+    nums1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     nums2 = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
     nums3 = ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29"]
     nums4 = ["30", "31", "32", "33", "34", "35", "36", "37", "38", "39"]
@@ -86,7 +86,7 @@ def generar_hoja_carton():
             check.sort()
 
             if check not in list_carton_general:
-                print(check)
+                # print(check)
                 list_carton_general.append(check)
             else:
                 print("repe")
